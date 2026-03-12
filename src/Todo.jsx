a384175d08e812ad7782e62f831206d07a404f3c
programmermosharof <mosharof.dev@gmail.com>
2026-03-12 15:33:22 +0600

    to do Conditional Rendering: If, Ternary learn Complete

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -1,8 +1,34 @@
-export default  function Todo({task, isDone}) {
-if(isDone === true){
-    return <li>Done : {task}</li>
-}else{
-    return <li>Pending: {task}</li>
-}
-}
+// export default  function Todo({task, isDone}) {
+// if(isDone === true){
+//     return <li>Done : {task}</li>
+// }else{
+//     return <li>Pending: {task}</li>
+// }
+// }
 
+// export default function Todo({task, isDone, time = 0}){
+//     return isDone ?
+//      <li>Done: {task} Time : {time}</li> : <li>Not Done: {task}</li>
+// }
+
+// export default function Todo({task, isDone, time = 0}){
+//     return isDone  &&
+//      <li>Done: {task} Time : {time}</li> 
+// }
+
+// export default function Todo({task, isDone, time = 0}){
+//     return isDone ||
+//      <li>Not Done: {task} Time : {time}</li> 
+// }
+
+
+export default function Todo({task, isDone, time = 0}){
+    let lisItem ;
+    if(isDone === true) {
+        lisItem= <li>Done: {task}</li>
+    }
+    else{
+        lisItem= <li>Pending: {task}</li>
+    }
+    return lisItem;
+}
